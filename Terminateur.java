fd55017d9679e2e9a15a78512a23aa660edb93d3
Francis Thibodeau <francis.thbd@gmail.com>
2015-02-19 16:31:12 -0500

    Presque finit
    POURQUOI LE Q MARCHE PAS !!!

--- Terminateur.java
+++ Terminateur.java
@@ -1,8 +1,36 @@
 // Terminateur.java
 // 
-// Fait par Melissa Boucher
+// Fait par Melissa Boucher et Francis Thibodeau 
 // 20 Fevrier 2015
-public class Terminateur
+import java.io.*;
+
+public class Terminateur implements Runnable
 {
-   
+	private static final String FIN = "Q";
+	boolean Condition = true;
+	
+	public void run()
+	{
+		String Key = null;
+		try
+		{
+			while(Condition)
+			{
+				BufferedReader reader = new BufferedReader(new InputStreamReader( System.in ) );
+				Key = reader.readLine();
+
+				String Clef = Key.trim();
+				
+            if(Key.toUpperCase().trim() == FIN)
+				{
+					Condition = false;
+				}
+			}
+		}
+		catch(IOException e)
+		{
+			System.err.println(e);
+			System.exit(1);
+		}
+	}
 }
--- TestTerminateur.java
+++ TestTerminateur.java
@@ -1,8 +1,37 @@
-// Terminateur.java
+// TestTerminateur.java
 // 
 // Fait par Melissa Boucher et Francis Thibodeau 
 // 20 Fevrier 2015
-public class TestTerminateur
+public class TestTerminateur 
 {
-   
+	Thread t;
+	public TestTerminateur()
+	{
+		Terminateur afficheur = new Terminateur();
+		t = new Thread(afficheur);
+		t.start();
+	}
+	
+	public void afficherPoint()
+	{
+		while (t.isAlive())
+		{
+			try
+    		{
+    		   System.out.print(".");
+    		   Thread.sleep(500);
+    		}
+    		catch(InterruptedException e)
+    		{
+    			System.err.println(e);
+    			System.exit(1);
+    		}
+		}
+	}
+	
+	public static void main(String args[])
+    {
+    	TestTerminateur app = new TestTerminateur();
+    	app.afficherPoint();
+    }
 }
